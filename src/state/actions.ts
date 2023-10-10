a16efaab51bffdba38208aa2e15f9b0128997c3c
Felizardo Ntswambeni Massimbe <f2massimbe@gmail.com>
2023-10-10 10:11:40 +0100

    feat: add move items logic

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -6,6 +6,13 @@ export type Action =
   | {
       type: "ADD_LIST";
       payload: string;
+    }
+  | {
+      type: "MOVE_LIST";
+      payload: {
+        draggedId: string;
+        hoverId: string;
+      };
     };
 
 export const addTask = (text: string, listId: string): Action => ({
@@ -16,4 +23,12 @@ export const addTask = (text: string, listId: string): Action => ({
 export const addList = (text: string): Action => ({
   type: "ADD_LIST",
   payload: text,
+});
+
+export const moveList = (draggedId: string, hoverId: string): Action => ({
+  type: "MOVE_LIST",
+  payload: {
+    draggedId,
+    hoverId,
+  },
 });
